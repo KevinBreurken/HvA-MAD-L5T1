06e358d40e13c3933c511c28bdc9748f97f5b4df
Kevin Breurken <kevinbreurken@gmail.com>
2022-05-22 21:33:31 +0200

    Cleared NoteViewModel

--- app/src/main/java/nl/hva/madlevel5task1/vm/NoteViewModel.kt
+++ app/src/main/java/nl/hva/madlevel5task1/vm/NoteViewModel.kt
@@ -13,44 +13,8 @@ import java.util.*
 
 class NoteViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val mainScope = CoroutineScope(Dispatchers.Main)
-
-    private val noteRepository = NoteRepository(application.applicationContext)
+    private val noteRepository =  NoteRepository(application.applicationContext)
 
     val note = noteRepository.getNotepad()
-
-    val error = MutableLiveData<String>()
-    val success = MutableLiveData<Boolean>()
-
-    fun updateNote(title: String, text: String) {
-
-        //if there is an existing note, take that id to update it instead of adding a new one
-        val newNote = Note(
-            id = note.value?.id,
-            title = title,
-            lastUpdated = Date(),
-            text = text
-        )
-
-        if (isNoteValid(newNote)) {
-            mainScope.launch {
-                withContext(Dispatchers.IO) {
-                    noteRepository.updateNotepad(newNote)
-                }
-                success.value = true
-            }
-        }
-    }
-
-    private fun isNoteValid(note: Note): Boolean {
-        return when {
-            note.title.isBlank() -> {
-                error.value = "Title must not be empty"
-                false
-            }
-            else -> true
-        }
-    }
-
 
 }
